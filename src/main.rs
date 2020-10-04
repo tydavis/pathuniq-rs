25e047ba409919b39d2073d893637c6989c508c7
Tyler Davis <tydavis@gmail.com>
2020-10-04 04:54:59 +0000

    almost working, need to figure out OK

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::collections::HashSet;
 use std::env;
 
 fn main() {
     let key = "PATH";
     match env::var_os(key) {
-        Some(val) => println!("{}: {:?}", key, val),
+        Some(path) => {
+            let mut paths = env::split_paths(&path).collect::<Vec<_>>();
+            let mut uniques = HashSet::new();
+            paths.retain(|e| uniques.insert(e.clone()));
+            let k = env::join_paths(paths);
+            println!("export PATH={:?}", k);
+        }
         None => println!("{} is not defined in the environment.", key),
     }
 }
